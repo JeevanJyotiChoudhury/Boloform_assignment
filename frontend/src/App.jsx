0b65c925dac8f692d26521b49a2d3b1e56c6022d
Jeevan Jyoti Choudhury <jeevanjyoti549@gmail.com>
2023-12-13 17:46:21 +0530

    error fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,12 @@
 import React from "react";
 import {Route, Routes } from "react-router-dom";
 import FormBuilder from "./Components/FormBuilder";
-import DisplayQuestions from "./Components/DisplayQuestions";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<FormBuilder/>} />
-        <Route path="/form-preview/:formId" element={<DisplayQuestions/>} />
       </Routes>
     </div>
   );
